a5eca77eeef75015ecdcc63192b830e4be1c07b2
Ronaldo Rodriguez <clerodri@espol.edu.ec>
2023-12-31 22:47:01 -0500

    primer commit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace BurguerDemo
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MainPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿
+namespace BurguerDemo
+{
+    public partial class MainPage : ContentPage
+    {
+  
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+     
+
+    }
+
+}
